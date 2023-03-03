030c84ea189b8d4fe3608039ccf68c4dbd4f8dc9
Kulenkov Sergei <kulenkovsd@gmail.com>
2023-03-03 21:37:02 +0900

    second commit

--- src/main/java/com/sergeikulenkov/spring/rest/App.java
+++ src/main/java/com/sergeikulenkov/spring/rest/App.java
@@ -1,13 +1,19 @@
 package com.sergeikulenkov.spring.rest;
 
+import com.sergeikulenkov.spring.rest.configuration.MyConfig;
+import com.sergeikulenkov.spring.rest.entity.Employee;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+        Communication communication = context.getBean("communication", Communication.class);
+        List<Employee> allEmployees = communication.getAllEmployees();
+        System.out.println(allEmployees);
     }
 }
